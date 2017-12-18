d5fed85c56dec34901b92eac0be84a664556030c
dianaman <diana.man.91@gmail.com>
2017-12-18 10:29:10 -0300

    scene style, icon

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image, View } from 'react-native';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm'; 
 import EmployeeList from './components/EmployeeList';
@@ -8,14 +9,20 @@ import EmployeeMessage from './components/EmployeeMessage';
 const RouterComponent = () => {
 	return(
 		<Router sceneStyle={{ paddingTop: 65 }}>
-			<Scene key="auth">
+			<Scene key="auth" 
+				navigationBarStyle={styles.navigationBarStyle} 
+				titleStyle={styles.titleStyle}>
 				<Scene key="login" component={LoginForm} title="Please login" />
 			</Scene>
-			<Scene key="main">
+			<Scene key="main" 
+				navigationBarStyle={styles.navigationBarStyle} 
+				titleStyle={styles.titleStyle}>
 				<Scene 
 					key="employeeList" 
 					component={EmployeeList} 
-					title="Employees"
+					renderLeftButton={()=> {return <AppLogo/>;}}
+					onLeft={()=>{}}
+					title="Manager"
 					rightTitle="Add"
 					onRight={() => Actions.employeeAdmin()}
 				/>
@@ -33,5 +40,22 @@ const RouterComponent = () => {
 		</Router>
 	);
 };
+
+const styles = {
+	navigationBarStyle: {
+		backgroundColor: '#333333'
+	},
+	titleStyle: {
+		color: 'white'
+	}
+}
+
+const AppLogo = () => {
+	return (
+		<View style={{alignItems:'center', flex:1, justifyContent:'center'}}>
+			<Image source={require('./manager-icon.png')} style={{width:35, height:35}}/>
+		</View>
+	);
+}
 
 export default RouterComponent;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const Header = (props) => {
-	const { textStyle, viewStyle } = styles;
-	return (
-		<View style={viewStyle}>
-			<Text style={textStyle}>{props.headerText}</Text>
-		</View>
-	);
-};
+class Header extends Component {
 
-const styles = {
-	viewStyle: {
-		backgroundColor: '#E8E8E8',
-		justifyContent: 'center',
-		alignItems: 'center',
-		height: 40,
-		shadowColor: '#000',
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.8
-	},
-	textStyle: {
-		fontSize: 20
+	render(){
+		const { textStyle, viewStyle } = this.styles;
+
+		return (
+			<View style={viewStyle}>
+				<Text style={textStyle}>{props.headerText}</Text>
+			</View>
+		);
 	}
-};
+
+	styles = {
+		viewStyle: {
+			backgroundColor: props.headerColor || '#E8E8E8',
+			justifyContent: 'center',
+			alignItems: 'center',
+			height: 40,
+			shadowColor: '#000',
+			shadowOffset: { width: 0, height: 2 },
+			shadowOpacity: 0.8
+		},
+		textStyle: {
+			fontSize: 20
+		}
+	};
+}
 
 export { Header };
